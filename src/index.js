962f8b2f58b55ad953ae944acd95c33f7b33ee65
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-30 22:16:38 +0300

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById('create-task-form');
+  const task_list = document.getElementById('tasks');
+
+  task_list.addEventListener('click', removeTask);//deleting task once clicked
+
+form.addEventListener('submit', add_task); //adding an eventlistener to my submit to listen for click
+  
+  function add_task(event){ //passing the eventlistener using the call back function
+    event.preventDefault(); //preventing html default
+
+   
+    const new_description = document.querySelector('#new-task-description').value;
+    //console.log(new_description);
+
+    const li = document.createElement('li'); //creating a list item
+
+    
+    li.appendChild(document.createTextNode(new_description)); // Adding text node with input value
+
+    const delete_btn = document.createElement('button'); //creating a delete button element
+
+    delete_btn.appendChild(document.createTextNode('Remove'));//appending the text node to my delete button
+
+    li.appendChild(delete_btn); // appending button to list item tag
+
+    task_list.appendChild(li);  // Appending list item tag to list
+
+    form.reset()// resets  the form after submiting task
+
+  }
+
+  function removeTask(event) {
+    if (event.target.nodeName === 'BUTTON') {
+      if(confirm('Confirm delete!')) {
+        let li = event.target.parentElement;
+        task_list.removeChild(li);
+      }
+    }
+
+  }
+
+
+})
+
